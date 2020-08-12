40e8b1be4d509c027c068280807ad2ed89ff21e0
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2020-08-12 10:38:10 +0900

    2442번 별 찍기 -5

---
+++ 2442_shooting_star_5.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for ( int i = 1; i <= n; i++ )
+	{
+		for ( int k = 1; k <= n - i; k++ )
+			printf(" ");
+		for ( int j = 1; j <= i * 2 - 1; j++ )
+			printf("*");
+		printf("\n");
+	}
+}
