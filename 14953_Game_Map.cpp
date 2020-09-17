6a8e5e2148fc3ec46838ef8f2ca8777b559cc27f
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2020-09-17 22:45:19 +0900

    14953번 Game Map

---
+++ 14953_Game_Map.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<vector<int>> vec;
+int dp[100001];
+
+int dfs(int cur)
+{
+	if ( dp[cur] )
+		return dp[cur];
+	int cnt=1;
+	for ( int i = 0; i < vec[cur].size(); i++ )
+	{
+		int next = vec[cur][i];
+		if ( vec[cur].size() < vec[next].size() )
+			cnt = max(dfs(next) + 1, cnt);
+	}
+		
+	return dp[cur]=cnt;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int a, b, MAX=0;
+	cin >> n >> m;
+	vec.resize(n + 1);
+	while(m-- )
+	{
+		cin >> a >> b;
+		vec[a].push_back(b);
+		vec[b].push_back(a);
+	}
+
+	for(int i=0;i<n;i++ )
+		MAX = max(MAX, dfs(i));
+
+	cout << MAX << "\n";
+	return 0;
+}
