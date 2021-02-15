611db8d595026579c119970e39c0d48f2e276d17
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-02-15 17:15:02 +0900

    1065번 한수

---
+++ 1065_hansoo.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, c = 0;
+	scanf("%d", &n);
+	for ( int i = 1; i <= n; i++ )
+		if ( i < 100 || (i / 100 + i % 10) == (i / 10 % 10 * 2) )
+			c++;
+	printf("%d", c);
+}
