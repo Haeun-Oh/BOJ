1d08cc884aed28cb84d3d1e1c562fd8305b2f4d2
Haeun-Oh <69044270+Haeun-Oh@users.noreply.github.com>
2021-02-17 13:58:01 +0900

    Update 1405_crazy_robot.cpp

--- 1405_crazy_robot.cpp
+++ 1405_crazy_robot.cpp
@@ -3,50 +3,47 @@
 #include <math.h>
 #include <vector>
 using namespace std;
- 
 int n;
 double ans;
 bool visit[30][30];
 double percent[4];
 int dx[4] = { 1, -1, 0, 0 };
 int dy[4] = { 0, 0, 1, -1 };
- 
 void dfs(int x, int y, double cnt)
 {
-    if ( n == 0 )
-    {
-        ans += cnt;
-        return;
-    }
- 
-    visit[y][x] = true;
-    for ( int i = 0; i < 4; i++ )
-    {
-        int nextx = x + dx[i];
-        int nexty = y + dy[i];
- 
-        if ( !visit[nexty][nextx] )
-        {
-            n--;
-            dfs(nextx, nexty, cnt*percent[i]);
-            n++;
-            visit[nexty][nextx] = false;
-        }
-    }
+	if ( n == 0 )
+	{
+		ans += cnt;
+		return;
+	}
+	visit[y][x] = true;
+	for ( int i = 0; i < 4; i++ )
+	{
+        if(percent[i]==0.0)
+            continue;
+		int nextx = x + dx[i];
+		int nexty = y + dy[i];
+        if(nextx<0 ||nexty<0||nextx>30||nexty>30)
+            continue;
+        
+		if ( !visit[nexty][nextx] )
+		{
+			n--;
+			dfs(nextx, nexty, cnt*percent[i]);
+			n++;
+			visit[nexty][nextx] = false;
+		}
+	}
 }
- 
 int main()
 {
-    scanf("%d", &n);
- 
-    for ( int i = 0; i < 4; i++ )
-    {
-        scanf("%lf", &percent[i]);
-        percent[i] *= 0.01;
-    }
- 
-    dfs(15, 15, 1.0);
- 
-    printf("%.10lf\n", ans);
-    return 0;
+	scanf("%d", &n);
+	for ( int i = 0; i < 4; i++ )
+	{
+		scanf("%lf", &percent[i]);
+		percent[i] *= 0.01;
+	}
+	dfs(15, 15, 1.0);
+	printf("%.9lf\n", ans);
+	return 0;
 }
